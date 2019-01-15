d037483ee6e5f005c82612a5158108781ab65d45
igor <igorzurawinski4@gmail.com>
2019-01-15 19:43:51 +0100

    Boty

---
+++ bot1.py
@@ -0,0 +1,16 @@
+import praw
+
+reddit = praw.Reddit('bot1')
+
+
+subreddit = reddit.subreddit("learnpython")
+  
+
+for submission in subreddit.hot(limit=5):
+    print(dir(submission))
+    
+    print("Title: ", submission.title)
+    print("Text: ", submission.selftext)
+    print("Score: ", submission.score)
+    print("---------------------------------\n")
+    
